3f4692f414284aa2f0f252f72790b3008abe2215
MsDiala <diala-is-diala@hotmail.com>
2024-03-30 18:13:37 +0000

    d

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Convert the word to lowercase for case insensitivity
+  word = word.toLowerCase();
+
+  // Initialize pointers for the start and end of the word
+  let start = 0;
+  let end = word.length - 1;
+
+  // Iterate over the word until the pointers meet
+  while (start < end) {
+    // If characters at the pointers don't match, it's not a palindrome
+    if (word[start] !== word[end]) {
+      return false;
+    }
+    // Move the pointers towards the center
+    start++;
+    end--;
+  }
+
+  // If the loop completes without returning false, it's a palindrome
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
+// Test cases
+console.log("Expecting: true");
+console.log("=>", isPalindrome("madam"));
 
-/*
-  Add written explanation of your solution here
-*/
+console.log("");
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
